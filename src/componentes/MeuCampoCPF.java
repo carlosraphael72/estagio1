b56d526f8ec2d1f0f5e91ce81bf2c761d6ae4d7e
Carlos Raphael <carlosraphael727@gmail.com>
2019-10-28 17:53:30 -0300

    Pessoa Fisica e Juridica

---
+++ src/componentes/MeuCampoCPF.java
@@ -0,0 +1,57 @@
+
+package componentes;
+
+import javax.swing.JFormattedTextField;
+import javax.swing.text.MaskFormatter;
+
+/**
+ *
+ * @author RAPHAEL
+ */
+public class MeuCampoCPF extends JFormattedTextField implements MeuComponente {
+    public boolean obrigatorio;
+    public String dica;
+    
+    public MeuCampoCPF(boolean obrigatorio, String dica){
+        try{
+           this.obrigatorio = obrigatorio;
+           this.dica = dica;
+           MaskFormatter mf = new MaskFormatter("###.###.###-##");
+           mf.install(this);
+           
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void limpar() {
+        setText("   .   .   -  ");
+    }
+
+    @Override
+    public void habilitar(boolean status) {
+        setEnabled(status);
+    }
+
+    @Override
+    public boolean eObrigatorio() {
+        return obrigatorio;
+    }
+
+    @Override
+    public boolean eValido() {
+        return true;
+    }
+
+    @Override
+    public boolean eVazio() {
+        return getText().equals("   .   .   -  ");
+    }
+
+    @Override
+    public String getDica() {
+        return dica;
+    }
+    
+}
--- src/telas/TelaGerenciarPessoa.java
+++ src/telas/TelaGerenciarPessoa.java
@@ -2,9 +2,13 @@
 package telas;
 
 import componentes.MeuCampoCEP;
+import componentes.MeuCampoCNPJ;
+import componentes.MeuCampoCPF;
 import componentes.MeuCampoData;
 import componentes.MeuCampoTelefone;
 import componentes.MeuCampoTexto;
+import java.awt.event.ActionEvent;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 
@@ -13,6 +17,7 @@ import javax.swing.JLabel;
  * @author RAPHAEL
  */
 public class TelaGerenciarPessoa extends TelaCadastro {
+    //Pessoa
     private MeuCampoTexto campoEndereco = new MeuCampoTexto(20, true, "Endereço");
     private MeuCampoTexto campoBairro = new MeuCampoTexto(5, true, "Bairro");
     private MeuCampoTexto campoComplemento = new MeuCampoTexto(10, true, "Complemento");
@@ -22,6 +27,19 @@ public class TelaGerenciarPessoa extends TelaCadastro {
     private MeuCampoTelefone campoTelefone2 = new MeuCampoTelefone(false, "Telefone 2");
     private MeuCampoTexto campoEmail = new MeuCampoTexto(20, true, "Email");
     private MeuCampoData campoDataInclusao = new MeuCampoData(true, "Data de inclusão");
+    //JCheckBox Tipo de Pessoa
+    private JCheckBox jcbPessoaFisica = new JCheckBox("Pessoa Fisica");
+    private JCheckBox jcbPessoaJuridica = new JCheckBox("Pessoa Jurídica");
+    //Pessoa Fisica
+    private MeuCampoTexto campoNome = new MeuCampoTexto(20, true, "Nome");
+    private MeuCampoData campoDataNascimento = new MeuCampoData(true, "Data de nascimento");
+    private MeuCampoCPF campoCPF = new MeuCampoCPF(true, "CPF");
+    private MeuCampoTexto campoMae = new MeuCampoTexto(20, true, "Nome da mãe");
+    //Pessoa Juridica
+    private MeuCampoTexto campoRazaoSocial = new MeuCampoTexto(20, true, "Razão Social");
+    private MeuCampoTexto campoFantasia = new MeuCampoTexto(20, true, "Nome Fantasia");
+    private MeuCampoCNPJ campoCNPJ = new MeuCampoCNPJ(true, "CNPJ");
+    private MeuCampoTexto campoInscricaoEstadual = new MeuCampoTexto(10, true, "Inscrição Estadual");
     
     
     public TelaGerenciarPessoa(){
@@ -113,5 +131,129 @@ public class TelaGerenciarPessoa extends TelaCadastro {
         gbc.gridx = 7;
         gbc.gridy = 2;
         jpComponentes.add(campoDataInclusao, gbc);
+        
+        //Tipo de Pessoa
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        jpComponentes.add(jcbPessoaFisica, gbc);
+        jcbPessoaFisica.addActionListener(this);
+        
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        jpComponentes.add(jcbPessoaJuridica, gbc);
+        jcbPessoaJuridica.addActionListener(this);
+        
+        //Pessoa Fisica
+        JLabel jlNome = new JLabel(campoNome.getDica());
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        jpComponentes.add(jlNome, gbc);
+            
+        gbc.gridx = 1;
+        gbc.gridy = 4;
+        jpComponentes.add(campoNome, gbc);
+        campoNome.habilitar(false);
+        
+        JLabel jlDataNascimento = new JLabel(campoDataNascimento.getDica());
+        gbc.gridx = 2;
+        gbc.gridy = 4;
+        jpComponentes.add(jlDataNascimento, gbc);
+        
+        gbc.gridx = 3;
+        gbc.gridy = 4;
+        jpComponentes.add(campoDataNascimento, gbc);
+        campoDataNascimento.habilitar(false);
+        
+        JLabel jlCPF = new JLabel(campoCPF.getDica());
+        gbc.gridx = 4;
+        gbc.gridy = 4;
+        jpComponentes.add(jlCPF, gbc);
+        
+        gbc.gridx = 5;
+        gbc.gridy = 4;
+        jpComponentes.add(campoCPF, gbc);
+        campoCPF.habilitar(false);
+        
+        JLabel jlMae = new JLabel(campoMae.getDica());
+        gbc.gridx = 6;
+        gbc.gridy = 4;
+        jpComponentes.add(jlMae, gbc);
+        
+        gbc.gridx = 7;
+        gbc.gridy = 4;
+        jpComponentes.add(campoMae, gbc);
+        campoMae.habilitar(false);
+        
+        //Pessoa Juridica
+        JLabel jlRazaoSocial = new JLabel(campoRazaoSocial.getDica());
+        gbc.gridx = 0;
+        gbc.gridy = 6;
+        jpComponentes.add(jlRazaoSocial, gbc);
+        
+        gbc.gridx = 1;
+        gbc.gridy = 6;
+        jpComponentes.add(campoRazaoSocial, gbc);
+        campoRazaoSocial.habilitar(false);
+        
+        JLabel jlFantasia = new JLabel(campoFantasia.getDica());
+        gbc.gridx = 2;
+        gbc.gridy = 6;
+        jpComponentes.add(jlFantasia, gbc);
+        
+        gbc.gridx = 3;
+        gbc.gridy = 6;
+        jpComponentes.add(campoFantasia, gbc);
+        campoFantasia.habilitar(false);
+        
+        JLabel jlCNPJ = new JLabel(campoCNPJ.getDica());
+        gbc.gridx = 4;
+        gbc.gridy = 6;
+        jpComponentes.add(jlCNPJ, gbc);
+        
+        gbc.gridx = 5;
+        gbc.gridy = 6;
+        jpComponentes.add(campoCNPJ, gbc);
+        campoCNPJ.habilitar(false);
+        
+        JLabel jlInscricaoEstadual = new JLabel(campoInscricaoEstadual.getDica());
+        gbc.gridx = 6;
+        gbc.gridy = 6;
+        jpComponentes.add(jlInscricaoEstadual, gbc);
+        
+        gbc.gridx = 7;
+        gbc.gridy = 6;
+        jpComponentes.add(campoInscricaoEstadual, gbc);
+        campoInscricaoEstadual.habilitar(false);
+    }
+    
+    public void actionPerformed(ActionEvent ae){
+        if(ae.getSource() == jcbPessoaFisica){
+            jcbPessoaJuridica.setSelected(false);
+            camposPessoaFisica(true);
+            camposPessoaJuridica(false);
+            
+        
+            
+        } else if(ae.getSource() == jcbPessoaJuridica){
+            jcbPessoaFisica.setSelected(false);
+            camposPessoaFisica(false);
+            camposPessoaJuridica(true);
+            
+            
+        }
+    }
+    
+    public void camposPessoaFisica(boolean status){
+        campoNome.habilitar(status);
+            campoDataNascimento.habilitar(status);
+            campoCPF.habilitar(status);
+            campoMae.habilitar(status);
+    }
+    
+    public void camposPessoaJuridica(boolean status){
+        campoRazaoSocial.habilitar(status);
+        campoFantasia.habilitar(status);
+        campoCNPJ.habilitar(status);
+        campoInscricaoEstadual.habilitar(status);
     }
 }
